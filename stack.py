77da9729e11c0886eef773fa27fc3c709f5c8826
Dai-stack <everygannaalright@gmail.com>
2025-11-01 08:45:00 +0900

    firstCommirt

---
+++ stack.py
@@ -0,0 +1,19 @@
+# stack
+st = []
+
+st.append(300)
+st.append(200)
+st.append(100)
+st.append(500)
+st.append(200)
+
+print(st)
+
+print("pop ->", st.pop())
+print("pop ->", st.pop())
+
+print(st)
+
+print("pop ->", st.pop(0))
+
+print(st)
