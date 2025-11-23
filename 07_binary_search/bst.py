def2e502885389a368f219432df3b4fd6bf7c808
Dai-stack <everygannaalright@gmail.com>
2025-11-23 18:39:38 +0900

    add:BST_adv

--- 07_binary_search/bst.py
+++ 07_binary_search/bst.py
@@ -17,7 +17,7 @@ class BST:
         self.preorder(node.left)
         self.preorder(node.right)
 
-    # 通りがけ順走査()
+    # 通りがけ順走査
     def inorder(self, node):
         if node is None:
             return
@@ -33,50 +33,86 @@ class BST:
         self.postorder(node.right)
         print(node.key, end=" ")
 
-    # BSTを表示する
+    # BSTを表示する①
     def display(self, node, levels=0):
         if node is None:
             return
 
         self.display(node.right, levels + 1)
-
         print("----" * 4 * levels + str(node.key))
-
         self.display(node.left, levels + 1)
 
+    # BSTを表示する②
+    def display_r(self, node, levels=0):
+        if node is None:
+            return
 
-# レベルゼロ（ルート）
-root = Node(40)
+        self.display(node.left, levels + 1)
+        print("----" * 4 * levels + str(node.key))
+        self.display(node.right, levels + 1)
 
-# レベル１
-root.left = Node(30)
-root.right = Node(70)
+    # 最小ノードを探索
+    def find_min_node(self, node):
+        while node.left:
+            node = node.left
+        return node.key
 
-# レベル2
-root.left.left = Node(10)
-root.left.right = None
-root.right.left = Node(60)
-root.right.right = Node(90)
+    # 最大ノードを探索
+    def find_max_node(self, node):
+        while node.right:
+            node = node.right
+        return node.key
 
-# レベル3
-root.left.left.left = None
-root.left.left.right = Node(20)
-root.right.left.left = None
-root.right.left.right = None
-root.right.right.left = None
-root.right.right.right = None
+    # ノード数を数える
+    def find_tree_size(self, node):
+        count = 0
+        if node is None:
+            return
+        self.find_tree_size(node.left)
+        self.find_tree_size(node.right)
+        return count
 
 
-bst = BST()
+if __name__ == "__main__":
+    # レベルゼロ（ルート）
+    root = Node(40)
 
-bst.display(root)
+    # レベル１
+    root.left = Node(30)
+    root.right = Node(70)
 
-bst.preorder(root)
+    # レベル2
+    root.left.left = Node(10)
+    root.left.right = None
+    root.right.left = Node(60)
+    root.right.right = Node(90)
 
-print("\n")
+    # レベル3
+    root.left.left.left = None
+    root.left.left.right = Node(20)
+    root.right.left.left = None
+    root.right.left.right = None
+    root.right.right.left = None
+    root.right.right.right = None
 
-bst.inorder(root)
+    bst = BST()
 
-print("\n")
+    bst.display(root)
 
-bst.postorder(root)
+    print("行き掛け順:")
+    bst.preorder(root)
+
+    print("\n")
+
+    print("通り掛け順:")
+    bst.inorder(root)
+
+    print("\n")
+    print("帰り掛け順:")
+    bst.postorder(root)
+
+    print("\n")
+    print("最小ノード値:", bst.find_min_node(root))
+
+    print("\n")
+    print("最大ノード値:", bst.find_max_node(root))
---
+++ 07_binary_search/bst_prac.py
@@ -0,0 +1,42 @@
+# 章末問題用
+import bst
+
+## bstを構築(問題7.4)
+root = bst.Node(25)
+
+root.left = bst.Node(6)
+root.right = bst.Node(49)
+
+root.left.left = bst.Node(2)
+root.right.left = bst.Node(28)
+root.right.right = bst.Node(65)
+
+root.right.right.left = bst.Node(51)
+root.right.right.right = bst.Node(66)
+
+root.right.right.right.right = bst.Node(78)
+
+root.right.right.right.right.left = bst.Node(88)
+
+bst = bst.BST()
+
+bst.display(root)
+
+print("\n")
+print("行き掛け順:")
+
+bst.preorder(root)
+
+print("\n")
+print("通り掛け順:")
+bst.inorder(root)
+
+print("\n")
+print("帰り掛け順:")
+bst.postorder(root)
+
+print("\n")
+print("最小ノード値:", bst.find_min_node(root))
+
+print("\n")
+print("最大ノード値:", bst.find_max_node(root))
